ba2f6d7f9f0d4be499cdb6ec795b7aa50b70f694
Alan Chu <alan_chu@yahoo.com>
2019-07-23 17:48:11 +1000

    Sign up, login, logout

--- scripts/auth.js
+++ scripts/auth.js
@@ -12,10 +12,40 @@ signupForm.addEventListener('submit', (e) => {
   // cred is the user credential returned by the function and 
   // available for use inside the callback function
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
     // close the signup modal & reset form
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
   });
+});
+
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  // prevent page reloading 
+    e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+
 });
