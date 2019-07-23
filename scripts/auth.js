59516bde7e1627a6a4c19865fb1fcb38530d4e1a
Alan Chu <alan_chu@yahoo.com>
2019-07-23 18:09:25 +1000

    Authorization state tracker

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,51 +1,52 @@
-// signup
-const signupForm = document.querySelector('#signup-form');
-signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
-
-  // sign up the user
-  // --------------Asynchronous function--------------|-trigger callback function when done --//
-  // cred is the user credential returned by the function and 
-  // available for use inside the callback function
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    // close the signup modal & reset form
-    const modal = document.querySelector('#modal-signup');
-    M.Modal.getInstance(modal).close();
-    signupForm.reset();
-  });
-});
-
-
-// logout
-const logout = document.querySelector('#logout');
-logout.addEventListener('click', (e) => {
-  e.preventDefault();
-  auth.signOut().then(() => {
-    console.log('user signed out');
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log('user logged in: ', user);
+    } else {
+      console.log('user logged out');
+    }
   })
-});
-
-// login
-const loginForm = document.querySelector('#login-form');
-loginForm.addEventListener('submit', (e) => {
-  // prevent page reloading 
-    e.preventDefault();
   
-  // get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
-
-  // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
-    // close the signup modal & reset form
-    const modal = document.querySelector('#modal-login');
-    M.Modal.getInstance(modal).close();
-    loginForm.reset();
+  // signup
+  const signupForm = document.querySelector('#signup-form');
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+  
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+      // close the signup modal & reset form
+      const modal = document.querySelector('#modal-signup');
+      M.Modal.getInstance(modal).close();
+      signupForm.reset();
+    });
   });
-
-});
+  
+  // logout
+  const logout = document.querySelector('#logout');
+  logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+  });
+  
+  // login
+  const loginForm = document.querySelector('#login-form');
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+  
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+      // close the signup modal & reset form
+      const modal = document.querySelector('#modal-login');
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+    });
+  
+  });
